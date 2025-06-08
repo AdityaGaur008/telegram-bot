e04441baea637a2b8eaa8f76627c46f6eee53b93
AdityaGaur008 <gauradi355@gmail.com>
2025-06-08 13:48:50 +0530

    Fix view directory path in server.js for correct view rendering

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // View engine setup
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'views'));
 
 // Routes
 app.use('/admin', adminRoutes);
