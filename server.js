dae8d621d96ba71bf130eb5e38e264d5429556ff
AdityaGaur008 <gauradi355@gmail.com>
2025-06-08 16:47:45 +0530

    Add console log for views directory path in server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // View engine setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+console.log('Views directory:', path.join(__dirname, '../views'));
 
 // Routes
 app.use('/admin', adminRoutes);
